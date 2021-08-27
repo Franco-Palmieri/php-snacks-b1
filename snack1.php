b3fa1167906da3ab3527917c092a57da1c4a386d
Ncesecrede <francescopm95@gmail.com>
2021-08-27 16:13:48 +0100

    Snack PHP

--- snack1.php
+++ snack1.php
@@ -7,6 +7,30 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $partite = [
+            [
+                "casa" => "Lazio",
+                "ospite" => "Roma",
+                "puntiCasa" => 50,
+                "puntiOspite" => 0
+            ],
+            [
+                "casa" => "Foggia",
+                "ospite" => "Marsala",
+                "puntiCasa" => 30,
+                "puntiOspite" => 20
+            ],
+            [
+                "casa" => "Rieti",
+                "ospite" => "Latina",
+                "puntiCasa" => 44,
+                "puntiOspite" => 24
+            ],
+        ];
+        for($x = 0; $x < count($partite); $x++){
+            echo "<div>{$partite[$x]['casa']} - {$partite[$x]['ospite']} | {$partite[$x]['puntiCasa']} - {$partite[$x]['puntiOspite']}</div>";
+        }
+    ?>
 </body>
 </html>
