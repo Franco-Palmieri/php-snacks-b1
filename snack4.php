5e3aa3cc5ffb9015b30832cf8fbbabf10003f90a
Ncesecrede <francescopm95@gmail.com>
2021-08-28 09:59:31 +0100

    Snack PHP

--- snack4.php
+++ snack4.php
@@ -7,6 +7,31 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+
+    $classe = [
+        ['nome' => "Francesco",
+         'cognome' => "Palmieri",
+         'voti' => [5, 6, 4, 7]
+        ],
+        ['nome' => "Michele",
+         'cognome' => "Pagano",
+         'voti' => [3, 4, 1, 5]
+        ],
+        ['nome' => "Francesca",
+         'cognome' => "Monti",
+         'voti' => [8, 6, 3, 6]
+        ],  
+    ];
+
+    for ($x = 0; $x < count($classe); $x++){
+
+        $media = array_sum($classe[$x]['voti']) / count($classe[$x]['voti']);
+
+        echo "<div> {$classe[$x]['nome']} {$classe[$x]['cognome']} {$media} </div>";
+
+    }
+    ?>
     
 </body>
 </html>
