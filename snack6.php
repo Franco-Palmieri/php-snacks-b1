e3490ddc9e33a33fa1c535313d0d5187cdb0b18d
Ncesecrede <francescopm95@gmail.com>
2021-08-29 12:58:46 +0100

    Snack PHP

--- snack6.php
+++ snack6.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 <?php
  
@@ -31,27 +33,25 @@
          ]
      ]
  ];
-    //invio nella nuova variabili le chiavi teachers & pm
-
-    $arrayKeys = array_keys($db);
-    
-    for($x = 0; $x <count($arrayKeys); $x++){
-
-        //invio le chiavi interne alle chiavi di teachers & pm ad una nuova variabile
-        $internKeys = $arrayKeys[$x];
-        
-        echo "<div>{$internKeys}</div>";
-
-        for($i = 0; $i < count($db[$internKeys]); $i++){
-
-            $details = $db[$internKeys][$i];
-            
-            // var_dump($details);
-            echo "<div>{$details['name']} {$details['lastname']}</div>";
-        }
-    }
-
 ?>
+
+    <div style="background-color: lightgray;">
+
+        <?php 
+            for($x = 0; $x < count($db['teachers']); $x++){
+                echo "<div> {$db['teachers'][$x]['name']} {$db['teachers'][$x]['lastname']} </div>";
+            }
+        ?>
+
+    </div>
+
+    <div style="background-color: lightgreen">
+            <?php
+                for($i = 0; $i < count($db['pm']); $i++){
+                    echo "<div> {$db['pm'][$i]['name']} {$db['pm'][$i]['lastname']} </div>";
+                }
+            ?>
+    </div>
     
 </body>
 </html>
